937cbb95ce010c96f8a55177c1415108c3f20c6c
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2026-06-16 19:40:18 -0700

    20260616-Update

--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -11,10 +11,16 @@ class PhysicsComputer():
         self.semimajor_axis                     =0
         self.eccentricity_vector                =[]
         self.right_ascension_of_ascending_node  =[]
+        self.argument_of_perigee                =0
+        self.true_anomaly                       =0
         self.unit_vector_k                      =[0,0,1] #Unit vector through the north pole
         self.unit_vector_i                      =[1,0,0] #Unit vector through principal direction
+        self.inclination                        =0
+        self.apoapsis                           =0
+        self.periapsis                          =0
+        self.remaining_fuel                     =0
+        self.rate_of_fuel_consumptuion          =0
         
-        self.inclination=0
     
     def calculate_vector_magnitude(self,vector):
         square_sum=0
@@ -96,21 +102,50 @@ class PhysicsComputer():
 
     
     def calculate_argument_of_perigee(self,ascending_node_vector):
-        pass
+        if len(ascending_node_vector)<3 or ascending_node_vector is None:
+            print("invalid ascending node vector")
+            return 0
+        else:
+            numerator=self.calculate_dot_product(ascending_node_vector,self.eccentricity_vector)
+            denominator=(self.calculate_vector_magnitude(ascending_node_vector))*(self.calculate_vector_magnitude(self.eccentricity_vector))
+            self.argument_of_perigee=math.acos(numerator/denominator)
     
-    def calculate_true_anomaly(self):
-        pass
     
+    def calculate_true_anomaly(self,position_vector):
+        if len(position_vector)<3 or position_vector is None:
+            print("invalid position vector")
+            return 0
+        else:
+            numerator=self.calculate_dot_product(self.eccentricity_vector,position_vector)
+            denominator =self.calculate_vector_magnitude(self.eccentricity_vector)*self.calculate_vector_magnitude(position_vector)
+            self.true_anomaly=math.acos(numerator/denominator)
     
+    def calculate_current_apoapsis_periapsis(self):
+        self.periapsis=self.semimajor_axis*(1-self.calculate_vector_magnitude(self.eccentricity_vector))
+        self.apoapsis =self.semimajor_axis*(1+self.calculate_vector_magnitude(self.eccentricity_vector))
     
-    def calculate_COE(self):
+    #This function will calculate the orbital parameters for every tick of the physics simulation
+    def calculate_instant_COE(self):
         self.calculate_specific_mechanical_energy()
         self.calculate_semimajor_axis()
-        self.calculate_eccentricity()
+        self.calculate_eccentricity_vector()
         self.calculate_orbital_inclination()
         self.calculate_right_ascension_of_ascending_node()
         self.calculate_argument_of_perigee()
         self.calculate_true_anomaly()
+        self.calculate_current_apoapsis_periapsis()
+    
+    def set_initial_conditions(self,remaining_fuel,rate_of_consumption):
+        self.remaining_fuel=remaining_fuel
+        self.rate_of_fuel_consumptuion=rate_of_consumption 
+    
+    
+    #Project telemetry data in future
+    #need a way to calculate fuel consumption rate
+    #need a way to transmit initial conditions. Should I start the simulation from telemetry or from derivation?
+    def calculate_current_trajectory():
+        pass
+        
     
     
     
